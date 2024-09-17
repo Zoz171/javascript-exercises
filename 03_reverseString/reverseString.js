bd2da7221b6f8f34b8d19cd439186460a00a946a
Zoz171 <zizo15.m@gmail.com>
2024-09-17 18:56:00 +0300

    Passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayOfRevChar = string.split('').reverse()
+    let reversedString = arrayOfRevChar.join('')
+    return reversedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+reverseString("hello");
